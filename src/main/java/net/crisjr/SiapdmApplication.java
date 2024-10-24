fe1a59f1af70f539e2cbbe6db6bddadd8c500b17
jrCris96 <cristianjavierquispech@mail.com>
2024-10-24 08:42:27 -0400

    añadiendo el archivo inicio del proyecto siapdm

---
+++ src/main/java/net/crisjr/SiapdmApplication.java
@@ -0,0 +1,13 @@
+package net.crisjr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiapdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiapdmApplication.class, args);
+	}
+
+}
